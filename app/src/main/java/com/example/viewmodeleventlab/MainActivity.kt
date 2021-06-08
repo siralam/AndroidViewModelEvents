a74c72d931ad3d970b3f88c83334b9b965150cc6
Sira <sira.lam@crypto.com>
2021-06-08 15:47:55 +0800

    Changed to use Fragment

--- app/src/main/java/com/example/viewmodeleventlab/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeleventlab/MainActivity.kt
@@ -7,26 +7,8 @@ import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnCallApi: Button
-    private val vm: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnCallApi = findViewById(R.id.btnCallApi)
-        btnCallApi.setOnClickListener {
-            vm.callSomeApi()
-        }
-
-        vm.apiResponse.observe(this) {
-            if (!it.isNullOrBlank()) {
-                showDialog(it)
-            }
-        }
-    }
-
-    private fun showDialog(message: String) {
-        CustomDialogFragment(message).show(supportFragmentManager, null)
     }
 }
---
+++ app/src/main/java/com/example/viewmodeleventlab/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.example.viewmodeleventlab
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class MainFragment: Fragment() {
+
+    private val vm: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCallApi = view.findViewById<Button>(R.id.btnCallApi)
+        btnCallApi.setOnClickListener {
+            vm.callSomeApi()
+        }
+
+        vm.apiResponse.observe(viewLifecycleOwner) {
+            if (!it.isNullOrBlank()) {
+                showDialog(it)
+            }
+        }
+    }
+
+    private fun showDialog(message: String) {
+        CustomDialogFragment(message).show(childFragmentManager, null)
+    }
+}
